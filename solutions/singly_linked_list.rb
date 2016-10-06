9b8fff0fa81481629c558223020909d765968d89
bgveenstra <b.g.veenstra@gmail.com>
2016-10-06 08:48:10 -0700

    ruby tests skeletongs

---
+++ solutions/singly_linked_list.rb
@@ -0,0 +1,9 @@
+# solutions/singly_linked_list.rb
+
+require_relative './node.rb'
+
+class SinglyLinkedList
+  attr_reader :head
+  def initialize(head=nil)
+  end
+end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,12 +1,57 @@
-require_relative '../models/singly_linked_list'
+# require_relative '../models/node'
+require_relative '../solutions/node'
 
 describe Node do
-  it 'allows accessing and setting data' do
+  let!(:node) { Node.new('hello') }
+
+  describe '#initialize' do
+    it 'sets data instance variable based on argument' do
+      expect(node.data).to eq('hello')
+    end
+    it 'raises an ArgumentError if argument is nil' do
+      expect { Node.new(nil) }.to raise_error ArgumentError
+    end
+    it 'sets next instance variable to nil' do
+      expect(node.next).to be_nil
+    end
+  end
+  describe '#data' do
+    it 'allows accessing data' do
+      expect(node).to respond_to(:data)
+      expect(node.data).to eq('hello')
+    end
   end
 
-  it 'allows accessing and setting next' do
+  describe '#data=' do
+    it 'takes in an argument' do
+      expect(node).to respond_to(:data=).with(1).argument
+    end
+    it 'sets data to the argument if it is not nil' do
+      node.data = 4
+      expect(node.data).to eq(4)
+    end
+    it 'raises an ArgumentError if the argument is nil' do
+      expect { node.data=nil }.to raise_error ArgumentError
+    end
   end
 
-  it 'validates that next is either a Node or nil' do
+  describe '#next' do
+    it 'allows accessing next' do
+      expect(node).to respond_to(:next)
+    end
+  end
+
+  describe '#next=' do
+    let!(:other_node) { Node.new('other node') }
+    it 'takes in an argument' do
+      expect(node).to respond_to(:next=).with(1).argument
+    end
+    it 'sets next if arugment is a Node or is nil' do
+      expect { node.next=other_node }.to change{node.next}.from(nil).to(other_node)
+      expect { node.next=nil }.to change{node.next}.to(nil)
+    end
+    it 'raises an ArgumentError if next is not either a Node or nil' do
+      expect { node.next=8 }.to raise_error ArgumentError
+    end
   end
 end
